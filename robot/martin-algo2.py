952e0441c7a07094c6e4beda922dabbb6ab8777c
martinma425 <martinma425@gmail.com>
2021-12-17 17:44:37 -0500

    Update martin-algo2.py

--- robot/martin-algo2.py
+++ robot/martin-algo2.py
@@ -21,29 +21,33 @@ hub.light_matrix.show_image('HAPPY')
 
 EPSILON = 0.1
 GAMMA = 0.9
-LAMBDA = 0.3
+LAMBDA = 0.2
 ALPHA = 0.1
 
 INTERVAL = 0.5
 WIDE_ANGLE = 45
 NARROW_ANGLE = 15
-EPISODE_LEN = 90
+EPISODE_LEN = 50
 
 LR_SPEED = 45
 TAIL_SPEED = 25
 
 LR_DOM = list(range(0, 2))
 T_DOM = [-1, 1]
-YALL_DOM = list(range(-2, 3))
-TAIL_DOM = list(range(-1, 2))
+YAW_DOM = list(range(-2, 3))
 
 ############## history ################
+
+
 def reset_reward_history():
     global sum_yaw, sum_reward, history_memory
     sum_yaw = 0
     sum_reward = 0
     history_memory = [None] * EPISODE_LEN
+
+
 reset_reward_history()
+
 
 def update_reward_history(timestep, state, action, new_state, reward, yaw):
     global sum_yaw, sum_reward, history_memory
@@ -51,28 +55,30 @@ def update_reward_history(timestep, state, action, new_state, reward, yaw):
     sum_yaw += yaw * yaw
     sum_reward += reward
 
+
 def send_reward_history(episode=0):
     print({
-        "history": history_memory, 
-        "episode": episode, 
+        "history": history_memory,
+        "episode": episode,
         "yaw_avg": sqrt(sum_yaw) / EPISODE_LEN,
         "rew_avg": sum_reward / EPISODE_LEN
     })
-############## END history ################ 
+############## END history ################
 
-# TODO: add quantized tail angle to the state
-# S = YALL_DOM
-S = [(i, j) for i in YALL_DOM for j in TAIL_DOM]
+
+S = [(i, j) for i in YAW_DOM for j in T_DOM]
 A = [(i, j, k) for i in LR_DOM for j in LR_DOM for k in T_DOM]
 for i in reversed(range(len(A))):
-    if A[i][0]==A[i][1]:
+    if A[i][0] == A[i][1]:
         A.pop(i)
 A_i = list(range(len(A)))
-Q = [[0]*len(A) for _ in range(len(S))]
+S_i = list(range(len(S)))
+Q = [[-random()]*len(A) for _ in range(len(S))]
 E = [[0]*len(A) for _ in range(len(S))]
 
+
 def epi_greedy(s):
-    if random()<EPSILON:
+    if random() < EPSILON:
         return randint(0, len(A)-1)
     else:
         max_i = 0
@@ -83,6 +89,7 @@ def epi_greedy(s):
                 max_i = i
         return max_i
 
+
 def do_action(a):
     l, r, t = a
 
@@ -90,7 +97,8 @@ def do_action(a):
     right_motor.run_at_speed(LR_SPEED*r)
     tail_motor.run_at_speed(TAIL_SPEED*t)
 
-def motors_reset(back): 
+
+def motors_reset(back):
     left_motor.run_at_speed(0)
     right_motor.run_at_speed(0)
     if back:
@@ -99,35 +107,34 @@ def motors_reset(back):
     right_motor_prime.run_to_position(70)
     if back:
         tail_motor_prime.run_to_position(0, direction='shortest path')
-    
-    
-def get_state(yall, tail):
-    t = 0
-    if (tail > WIDE_ANGLE):
-        if (tail > 270 - WIDE_ANGLE):
-            t = 1
-        else:
-            t = -1
-    
+
+
+def get_state(yaw, tail):
+    t = -1
     y = 0
-    if yall < -WIDE_ANGLE:
+
+    if (tail > 100):
+        t = 1
+    if yaw < -WIDE_ANGLE:
         y = -2
-    elif yall > WIDE_ANGLE:
+    elif yaw > WIDE_ANGLE:
         y = 2
-    elif yall < -NARROW_ANGLE:
+    elif yaw < -NARROW_ANGLE:
         y = -1
-    elif yall > NARROW_ANGLE:
+    elif yaw > NARROW_ANGLE:
         y = 1
 
     s = (y, t)
     return S.index(s)
 
-    
-def get_reward(yall):
-    return -exp(abs(yall/180.0))+1
+
+def get_reward(yaw):
+    return -exp(abs(yaw/180.0))+1
+
 
 def q_error(s, a, r, new_s):
     return r + GAMMA*max(Q[new_s]) - Q[s][a]
+
 
 def emote(r):
     if r < -1:
@@ -136,6 +143,7 @@ def emote(r):
         hub.light_matrix.show_image('SAD')
     else:
         hub.light_matrix.show_image('HAPPY')
+
 
 def Q_learning():
     episode = 0
@@ -147,41 +155,58 @@ def Q_learning():
         step = 0
         reset_reward_history()
         while step < EPISODE_LEN:
-            yall = hub.motion_sensor.get_yaw_angle()
+            yaw = hub.motion_sensor.get_yaw_angle()
             tail = tail_motor_prime.get_position()
-            s = get_state(yall, tail)
+            s = get_state(yaw, tail)
             a = epi_greedy(s)
             do_action(A[a])
             wait_for_seconds(INTERVAL)
-            new_yall = hub.motion_sensor.get_yaw_angle()
+            new_yaw = hub.motion_sensor.get_yaw_angle()
             new_tail = tail_motor_prime.get_position()
-            new_s = get_state(new_yall, new_tail)
-            r = get_reward(new_yall)
-            update_reward_history(step, s, a, new_s, r, new_yall)
+            new_s = get_state(new_yaw, new_tail)
+            r = get_reward(new_yaw)
+            update_reward_history(step, s, a, new_s, r, new_yaw)
             emote(r)
             Q[s][a] += ALPHA*q_error(s, a, r, new_s)
             step += 1
         send_reward_history(episode)
 
-Q_learning()
 
-# def et_q_error(s, a, new_s, new_a, r):
-#     return r + GAMMA*Q[new_s][new_a] - Q[s][a]
+def et_q_error(s, a, new_s, new_a, r):
+    return r + GAMMA*Q[new_s][new_a] - Q[s][a]
 
-# def et_Q_learning():
-#     while True:
-#         motors_reset(True)
-#         hub.left_button.wait_until_pressed()
-#         hub.motion_sensor.reset_yaw_angle()
-#         step = 0
-#         s = get_state(hub.motion_sensor.get_yaw_angle())
-#         a = epi_greedy(s)
-#         while step < EPISODE_LEN:
-#             do_action(A[a])
-#             wait_for_seconds(INTERVAL)
-#             theta = hub.motion_sensor.get_yaw_angle()
-#             new_s = get_state(theta)
-#             new_a = epi_greedy(new_s)
-#             r = get_reward(theta)
-#             emote(r)
-#             delta = et_q_error(s, a
+
+def sarsa_lambda():
+    while True:
+        motors_reset(True)
+        hub.left_button.wait_until_pressed()
+        hub.motion_sensor.reset_yaw_angle()
+        step = 0
+
+        yaw = hub.motion_sensor.get_yaw_angle()
+        tail = tail_motor_prime.get_position()
+        s = get_state(yaw, tail)
+        a = epi_greedy(s)
+
+        while step < EPISODE_LEN:
+            do_action(A[a])
+            wait_for_seconds(INTERVAL)
+            new_yaw = hub.motion_sensor.get_yaw_angle()
+            new_tail = tail_motor_prime.get_position()
+            new_s = get_state(new_yaw, new_tail)
+            new_a = epi_greedy(new_s)
+            r = get_reward(new_yaw)
+            emote(r)
+            delta = et_q_error(s, a, new_s, new_a, r)
+            E[s][a] += 1
+            for each_s in S_i:
+                for each_a in A_i:
+                    Q[each_s][each_a] += ALPHA * delta * E[each_s][each_a]
+                    E[each_s][each_a] = GAMMA * LAMBDA * E[each_s][each_a]
+            s = new_s
+            a = new_a
+            step += 1
+
+
+# Q_learning()
+sarsa_lambda()
